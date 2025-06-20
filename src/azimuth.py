6801d5bb94a63c839128e2a9a5af1ccc8fa2cbd4
Jordy Dal Corso <75387385+jdalcorso@users.noreply.github.com>
2025-06-20 10:09:56 +0100

    RCMC is now performed in freq domain. No more int casting and roll (i.e. now more precision!)

--- src/azimuth.py
+++ src/azimuth.py
@@ -14,13 +14,13 @@ def get_args_parser():
     parser.add_argument('--central_frequency', default = 20e6, type = float)
     parser.add_argument('--bandwidth', default = 10e6, type = float)
     parser.add_argument('--dielectric_eps', default = 3.15, type = float)
-    parser.add_argument('--sc_height', default = 315e3)
+    parser.add_argument('--sc_height', default = 251.5e3)
     parser.add_argument('--sc_vel', default = 3.397e3)
     parser.add_argument('--prf', default = 135)
     # Additional
-    parser.add_argument('--start', default = 400, type = int)
-    parser.add_argument('--stop', default = 500, type = int)
-    parser.add_argument('--aperture', default = 2, type = float, help='Aperture in seconds')
+    parser.add_argument('--start', default = 700, type = int)
+    parser.add_argument('--stop', default = 800, type = int)
+    parser.add_argument('--aperture', default = 8, type = float, help='Aperture in seconds')
     return parser
 
 
@@ -58,11 +58,14 @@ def focus(R, args):
 
     # Range Cell Migration Correction
     R_fft = np.fft.fftshift(np.fft.fft(R, axis = 1), axes = 1)
-    shifts = np.power(v * t, 2)/(2 * r) # m
-    shifts_in_pixels = -np.round(shifts/dx).astype(int)*10
-    R_fft_shifted = np.zeros_like(R_fft)
-    for i in range(rangelines):
-        R_fft_shifted[:,i] = np.roll(a = R_fft[:,i], shift = shifts_in_pixels[i], axis = 0)
+    shifts = np.power(v * t, 2) / (2 * r)
+    shifts_in_pixels = -shifts / dx
+    N = R.shape[0]
+    freq = np.fft.fftshift(np.fft.fftfreq(N, d=dx))
+    R_fft_shifted = np.zeros_like(R_fft, dtype=np.complex128)
+    for i in range(R.shape[1]):
+        phase_shift = np.exp(-2j * np.pi * freq * shifts_in_pixels[i])
+        R_fft_shifted[:, i] = R_fft[:, i] * phase_shift
 
     # Azimuth Compression
     k = -2 * v**2 / (lb * r)            # Slope of Doppler frequency
